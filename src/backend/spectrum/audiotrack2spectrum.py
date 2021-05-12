90f54bdcebade7ee20930473252078944ac528f7
alero <amoldovan@itba.edu.ar>
2021-05-12 12:46:07 -0300

    no se, digamos que hice el audiotrack2spectrum.py, pero es tan tonto y a la vez tan molesto de colocar en el principal, q hasta q no arregle la gui, no voy a poder probarlo. mejor hago eso :(

--- src/backend/spectrum/audiotrack2spectrum.py
+++ src/backend/spectrum/audiotrack2spectrum.py
@@ -1,9 +1,35 @@
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import matplotlib.pyplot as plt
+
+from matplotlib.mlab import window_hanning,window_none
+import numpy as np
+import scipy.signal as ss
+
 from src.backend.audio_tracks.audio_track import AudioTrack
+from typing import Callable
+
+
+def blackman(a: list) -> list:
+    return np.multiply(a,np.blackman(len(a)))
+
+
+def bartlett(a: list) -> list:
+    return np.multiply(a,np.bartlett(len(a)))
+
+
+def hamming(a: list) -> list:
+    return np.multiply(a,np.hamming(len(a)))
+
+
+def blackmanharris(a: list) -> list:
+    return np.multiply(a,ss.blackmanharris(len(a)))
 
 
 class AudioTrack2Spectrum(object):
     def __init__(self):
         print("AudioTrack2Spectrum created!")
 
-    def get_spectrum(self, audio_track:AudioTrack, config: list):
-        print("AudioTrack2Spectrum get_spectrum")
+    def draw_spectrum(self, audio_track: AudioTrack, Fs: float, NFFT:int, noverlap: int, window: Callable, axis):
+        print("AudioTrack2Spectrum draw_spectrum")
+        Pxx,freqs,bins,im = axis.specgram(audio_track.content,NFFT=NFFT,Fs=Fs,noverlap=noverlap,window=window)
