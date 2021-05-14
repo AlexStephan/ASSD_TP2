9b7e746d01ebe595fd39c3e60398dae44c2a74f3
MTripo <mtripoli@itba.edu.ar>
2021-05-14 07:41:40 -0300

    bokita el mas grande papa

--- src/backend/synthesis/sample_based_synthesis.py
+++ src/backend/synthesis/sample_based_synthesis.py
@@ -2,17 +2,87 @@ from src.backend.instruments.instrument_list import INSTRUMENT
 from src.backend.tracks.track import Track
 from src.backend.audio_tracks.audio_track import AudioTrack
 from src.backend.synthesis.synthesis_template import SynthesisTemplate, STATE
-
+import numpy as np
+import pytsmod
+from scipy.io.wavfile import read
 
 class SampleBasedSynthesis(SynthesisTemplate):
     def __init__(self):
         super().__init__()
+        self.instrument = ""
+        self.track_len = 0
+        self.track_pos = 0
+        self.note_freq = 0
+        self.note_start = 0
+        self.note_end = 0
+        self.note_vel = 0
+        self.synth_track = []
+
+        #Fixed
+        self.sample_array = []
+        self.instruments_array = ["Piano"]
+        self.sample_freq = {"Piano": 440}
 
     def synthesize_audio_track(self, track: Track, instrument: INSTRUMENT):
         super().synthesize_audio_track(track, instrument)
-        # Analizar si el instrumento escogido es compatible con este sintetizador
-        # y generar el audio track
-        # si no pudo hacerlo:
-        #   self.state = STATE.ERROR
-        # de lo contrario
         self.state = STATE.LOADED
+        if instrument not in self.instruments:
+            self.state = STATE.ERROR
+        else:
+            self.instrument = instrument
+        self.track_len = len(track)
+        while self.track_pos != self.track_len:
+            self.note_freq = self.get_note_freq()
+            self.note_start = int(self.get_note_start() * 44100)
+            self.note_end = int(self.get_note_end() * 44100)
+            self.note_vel = self.get_note_vel()
+            self.synthesize()
+
+            for n in self.end - self.start:
+                self.audio_track[self.start + n] += self.synth_track[n]
+                n += 1
+            self.track_pos += 1
+
+    def time_scale(self):
+        duration_ratio = (self.note_end - self.note_start) / self.get_sample_duration()
+        if duration_ratio == 1:
+            return self.synth_track
+        return np.transpose(pytsmod.phase_vocoder(self.sample_array, duration_ratio))
+
+    def pitch_shift(self):
+        note_freq = self.note_freq
+        shift_factor = note_freq / self.get_sample_freq()
+        if shift_factor == 1:
+            return self.synth_track
+        return self.speedx(np.transpose(pytsmod.phase_vocoder(self.sample_array, shift_factor)), shift_factor)
+
+    def speedx(self, sound_array, factor):                                # http://zulko.github.io/blog/2014/03/29/soundstretching-and-pitch-shifting-in-python/
+        """ Multiplies the sound's speed by some `factor` """
+        indices = np.round(np.arange(0, len(sound_array), factor))
+        indices = indices[indices < len(sound_array)].astype(int)
+        return sound_array[indices.astype(int)]
+
+    def get_note_freq(self):
+        return self.track[self.track_pos].frequency
+
+    def get_note_start(self):
+        return self.track[self.track_pos].start
+
+    def get_note_end(self):
+        return self.track[self.track_pos].end
+
+    def get_note_vel(self):
+        return self.track[self.track_pos].velocity
+
+    def synthesize(self):
+        self.synth_track = self.pitch_shift()
+        self.synth_track = self.time_scale()
+
+    def set_sample_array(self):
+        fs,  self.sample_array = read('..\\wav_files\\' + self.instrument + '.wav', mmap=False)
+
+    def get_sample_freq(self):
+        return self.sample_freq[self.instrument]
+
+    def get_sample_duration(self):
+        return len(self.sample_array)
